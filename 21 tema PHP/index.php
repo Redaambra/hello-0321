70c99dca73163181344c70aaff14fbaf0eba42ec
Redaambra <r.ambrazaityte@gmail.com>
2018-05-03 10:14:26 +0300

    20180503 10:14

---
+++ 21 tema PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <?php echo "<h2>Langas į gamtą</h2>";
+    echo "<footer>Metai:2000-".date("Y")."<footer>";
+    echo "<input type="text" name="name">";
+   
+    
+    
+    ?>
+</body>
+</html>
