d24b91b8609992f9ad3f73a6b9c581bb08a74b6e
Redaambra <r.ambrazaityte@gmail.com>
2018-05-10 13:57:58 +0300

    20180510 13:57

--- wordpress/wp-content/themes/balta tema/header.php
+++ wordpress/wp-content/themes/balta tema/header.php
@@ -25,15 +25,12 @@
   <body>
 
     <div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item active" href="#">Home</a>
-          <a class="blog-nav-item" href="#">New features</a>
-          <a class="blog-nav-item" href="#">Press</a>
-          <a class="blog-nav-item" href="#">New hires</a>
-          <a class="blog-nav-item" href="#">About</a>
-        </nav>
-      </div>
+        <div class="container">
+            <?php wp_nav_menu( array(
+            'theme_location' => 'primary',
+            'menu_class' => 'blog-nav',
+            'menu_id' => 'primary-menu' ) ); ?>
+        </div>
     </div>
 
     <div class="container">
