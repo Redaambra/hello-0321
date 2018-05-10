d30bbe8596dd3248bfc1cb51878a521e854e34db
Redaambra <r.ambrazaityte@gmail.com>
2018-05-10 14:47:02 +0300

    20180510 14:46

--- 22tema_PHP/22uzd/index.php
+++ 22tema_PHP/22uzd/index.php
@@ -4,14 +4,39 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Slaptažodžio tikrinimas</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="https://use.fontawesome.com/6603c9fd94.js"></script>
 </head>
 <body>
 <h1> Slaptažodžio atnaujinimas </h1>
-    <form action="login.php" method="post">
-        <input name='user' placeholder="login">
-        <input type='password' name='pass' placeholder="password">
-        <button>Login</button>
+    <form id="container" action="login.php" method="post">
+        <input type='password' name='pass1' placeholder="password">
+        <input type='password' name='pass2' placeholder="repeat password">
+        <button onclick="login()">Change pasword</button>
     </form>  
+
+    <p></p>
+
+
+<script>
+            function login() {
+            $('p').html('<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i><span class="sr-only">Connecting...</span>');
+
+            $.post('login.php', {
+                pass1: $('input[name="pass1"]').val(),
+                pass2: $('input[name="pass2"]').val()
+            }).done(function(resp) {
+                if (resp.success) {
+                    $('#container').empty();
+                    $('p').html('Pakeista');
+                    lentele();
+                } else {
+                    $('p').html(resp.error)
+                }
+            });
+        }
+
+        </script>
 </body>
 </html>
--- 22tema_PHP/22uzd/login.php
+++ 22tema_PHP/22uzd/login.php
@@ -1,3 +1,19 @@
 <?php
 
-?>
+sleep(5);
+
+header("Content-Type: application/json; charset=UTF-8");
+
+if ($_REQUEST['pass1'] == $_REQUEST['pass2']) {
+    $response = [
+        success => true,
+        token => 'Robotas irgi turi jausmus. T.y. slaptažodis pakeistas'
+    ];
+} else {
+    $response = [
+        success => false,
+        error => 'Slaptažodis nepakeistas :p'
+    ];
+}
+
+echo json_encode($response);
