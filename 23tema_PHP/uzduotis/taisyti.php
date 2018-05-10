ff629131829b7989f231906a35960eb9ae6ef647
Redaambra <r.ambrazaityte@gmail.com>
2018-05-10 11:22:14 +0300

    20180510 11:22

--- 23tema_PHP/uzduotis/taisyti.php
+++ 23tema_PHP/uzduotis/taisyti.php
@@ -13,3 +13,21 @@ header('Content-Type: application/json');
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 $id = $_REQUEST['id'];
+$data = $_REQUEST['date'];
+$numeris = $_REQUEST['number'];
+$kelias = $_REQUEST['distance'];
+$laikas = $_REQUEST['time'];
+
+// Suformuojame UPDATE užklausą
+$sql = "UPDATE radars SET number = ?, date = ?, distance = ?, time = ? WHERE id = ?";
+$stmt = $conn->prepare($sql);
+
+// Priskiriame parametrų reikšmes
+$stmt->bind_param("ssdd", $data, $numeris, $kelias, $laikas);
+
+// Vykdome UPDATE užklausą
+$stmt->execute();
+
+echo json_encode([success => true]);
+
+
